c118b3ee88c57180d89a823e46361a7d2fe5fc72
paginated <paginated@tutamail.com>
2024-10-13 17:58:34 +0530

    dotfiles: hm: foot: Set terminal font family to SauceCodePro Nerd Font

--- modules/home-manager/terminal/default.nix
+++ modules/home-manager/terminal/default.nix
@@ -5,6 +5,13 @@
     settings = {
       main = {
         term = "xterm-256color";
+
+        font = "SauceCodePro Nerd Font:size=10.8:style=SemiBold";
+        font-bold = "SauceCodePro Nerd Font:size=10.8:style=Bold";
+        font-italic = "SauceCodePro Nerd Font:size=10.8:style=Italic";
+        font-bold-italic = "SauceCodePro Nerd Font:size=10.8:style=Bold Italic";
+        line-height = "19";
+        underline-offset = "19";
       };
     };
   };
